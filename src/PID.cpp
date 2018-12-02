17ec2f9dc702f88fc48dcf1c0cdc478f31171131
marton.helli <marton.helli@aimotive.com>
2018-12-02 10:37:16 +0100

    update PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,16 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error; // diff_cte = cte - prev_cte
+  p_error = cte;
+  i_error += cte; // sum of all cte
 }
 
 double PID::TotalError() {
+  
 }
 
