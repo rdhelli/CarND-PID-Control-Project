ca9d848ca955a0fd6dfd0ae3ccc49e5659100349
marton.helli <marton.helli@aimotive.com>
2018-12-02 16:08:15 +0100

    update PID.cpp and main.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,7 +11,12 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
-  
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
+  PID::Kp = Kp;
+  PID::Ki = Ki;
+  PID::Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
@@ -21,6 +26,7 @@ void PID::UpdateError(double cte) {
 }
 
 double PID::TotalError() {
-  
+  double control value = - (Kp * p_error) - (Ki * i_error) - (Kd * d_error);
+  return control_value
 }
 
